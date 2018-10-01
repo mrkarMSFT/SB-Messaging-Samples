30aebea633c3976ea7a57fd103370ca615d2eb6b
mrkarMSFT <38073718+mrkarMSFT@users.noreply.github.com>
2018-10-01 21:31:32 +0530

    Add files via upload

---
+++ SessionQueueSend/SessionQueueSend/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessionQueueSend
+{
+    class Program
+    {
+        static string connectionString = "Endpoint=sb://<NS>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=";
+        static string queueName = "sessionaware";
+
+        static void Main(string[] args)
+        {
+            SendMessage().GetAwaiter().GetResult();
+        }
+
+        static async Task SendMessage()
+        {
+            var factory = MessagingFactory.CreateFromConnectionString(connectionString);
+            var sender = await factory.CreateMessageSenderAsync(queueName);
+            
+            for (int i = 0; i < 4000; i++)
+            {
+                await sender.SendAsync(new BrokeredMessage("Hello World - " + i) { MessageId = i.ToString(), SessionId = "Order" });
+                Console.WriteLine(i + " - Message Sent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
